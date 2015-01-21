63212b226eede36ecce5dc73d46b66d94362ac70
yshunnar <yshunnar@umich.edu>
2015-01-21 12:16:56 -0500

    First project Commit

---
+++ Assets/Assets/followPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class followPlayer : MonoBehaviour {
+
+	public Transform hero;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = new Vector3 (hero.position.x, this.transform.position.y, this.transform.position.z);
+	
+	}
+}
